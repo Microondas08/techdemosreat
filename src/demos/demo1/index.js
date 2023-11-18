c6682ed102fe301cf9c174d8fb2cf210a680cb18
Juan Carlos López Morales <97jclm@gmail.com>
2023-11-18 23:32:33 +0100

    login

--- src/demos/demo1/index.js
+++ src/demos/demo1/index.js
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Demo1 = () => {
-  return <p>Esta es la demo1</p>;
-};
+class Demo1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      predefinedUsername: "",
+      predefinedPassword: "",
+      showPassword: false,
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.validateLogin();
+  };
+
+  togglePasswordVisibility = () => {
+    this.setState((prevState) => ({
+      showPassword: !prevState.showPassword,
+    }));
+  };
+
+  setPredefinedUser = () => {
+    const { predefinedUsername, predefinedPassword } = this.state;
+    this.setState({
+      username: predefinedUsername,
+      password: predefinedPassword,
+    });
+  };
+
+  validateLogin = () => {
+    const { username, password, predefinedUsername, predefinedPassword } =
+      this.state;
+    if (username === predefinedUsername && password === predefinedPassword) {
+      console.log("Inicio de sesión exitoso");
+    } else {
+      console.log("Datos incorrectos");
+    }
+  };
+
+  render() {
+    const { showPassword } = this.state;
+    return (
+      <div>
+        <h2>Formulario de Inicio de Sesión</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Usuario:
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <br />
+          <label>
+            Contraseña:
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button onClick={this.togglePasswordVisibility}>
+            {showPassword ? "Ocultar" : "Mostrar"} Contraseña
+          </button>
+          <br />
+          <button type="submit">Iniciar Sesión</button>
+        </form>
+
+        <h2>Configurar Usuario Predefinido</h2>
+        <label>
+          Usuario Predefinido:
+          <input
+            type="text"
+            name="predefinedUsername"
+            value={this.state.predefinedUsername}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Contraseña Predefinida:
+          <input
+            type="password"
+            name="predefinedPassword"
+            value={this.state.predefinedPassword}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <button onClick={this.setPredefinedUser}>
+          Establecer Usuario Predefinido
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Demo1;
